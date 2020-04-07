0d25c4360b058040a08c53c377b34582e8d7288f
Alex Shulyaev <shulyaev@gmail.com>
2020-04-08 00:38:28 +0300

    Add about text

--- public/js/app.js
+++ public/js/app.js
@@ -6,7 +6,7 @@ randomForm.addEventListener("submit", (e) => {
 
   message.textContent = "Loading...";
 
-  fetch("http://puzzle.mead.io/puzzle").then((response) => {
+  fetch("https://puzzle.mead.io/puzzle").then((response) => {
     response.json().then((data) => {
       message.textContent = data.puzzle;
     });
